47601d0d3f758babc949786257e5d98e65a15c67
Charles Wright <cw@384.co>
2024-10-09 15:37:27 -0500

    Touch-up because I forgot to push the template :facepalm:

--- Formula/os384-cli.rb
+++ Formula/os384-cli.rb
@@ -10,15 +10,8 @@ class Os384Cli < Formula
     depends_on "deno"
   
     def install
-      # Loop over each matching filename
-      Dir["src/*.ts"].each do |file|
-        # Extract the base filename (e.g., "authorize.channel.ts")
-        base_name = File.basename(file)
-        # Prepend "384." to the base filename (e.g., "384.authorize.channel.ts")
-        new_name = "384." + base_name
-        # Install the file under the new name
-        bin.install file => new_name
-      end
+      # Directly install executable .ts scripts into bin
+      bin.install Dir["src/*.ts"]
     
       # Ensure scripts have executable permissions
       chmod 0755, Dir["#{bin}/*.ts"]
